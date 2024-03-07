0bab5c8de515ef5278b38ded8d9a934b19ef83b3
kamiji <kamiji.y.3@gmail.com>
2024-03-07 11:22:22 +0900

    ジャンプ追加

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rigid2D;
+    float jumpForce = 680.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+        this.rigid2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ジャンプする
+        if(Input.GetKeyDown(KeyCode.Space))
+    {
+    this.rigid2D.AddForce(transform.up*this.jumpForce);
+    }
+  }
+}
